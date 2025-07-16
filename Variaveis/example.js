c71f63de9b3bc4aef81579f4a62b1e3fc5ea402c
Yuri <yuri.anderson.lobo@gamail.com>
2025-07-15 23:12:01 -0300

    masteclass - variaveis

---
+++ Variaveis/example.js
@@ -0,0 +1,20 @@
+if(true){
+var pi = 3.141592;//quando usamos var ela tem escopo global mesmo estando na funcao
+}
+console.log(pi);
+var pi= 3; //redeclaraçao como se fosse uma redeclaracao
+console.log(pi);
+
+let pi1=3.1415;
+console.log(pi1);
+
+pi1=3;//reatribuicao
+console.log(pi1);
+
+// if(true){
+//     let pi2 = 3.141592;//quando usamos var ela tem escopo globallocal  estando na funcao
+//     }
+//     console.log(pi2);
+
+const pi3 =3.14
+console.log(pi3)
